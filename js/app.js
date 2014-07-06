cd35850c66704b97d25c7172b17e82b23b51dbc9
Ivo Šnábl <snabl@iba.muni.cz>
2014-07-06 14:48:09 +0200

    Testy se zkopírovanými formuláři

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+//$('.off-canvas-wrap').foundation('offcanvas', 'open', 'move-right');
+//$('.off-canvas-wrap').foundation('offcanvas', 'open');
+
+// otevre offcanvas
+//$('.off-canvas-wrap').foundation('offcanvas', 'toggle', 'move-right'); // predelat podle cookie
+// if scroll ... .icon-bar addClass('fixed') + addClass('contain-to-grid')
